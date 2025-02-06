1157150b4faed58aa4260d3fe5b3a4510ae9b03b
Etczer <etczermerivil@gmail.com>
2025-02-06 15:09:46 -0500

    npm run build

--- frontend/src/components/VideoSection/VideoSection.jsx
+++ frontend/src/components/VideoSection/VideoSection.jsx
@@ -20,13 +20,15 @@ const VideoSection = () => {
       <div className="movie-container">
         <h2 className="movie-title">View Our Technology in Action</h2>
         <div className="movie-video">
-          <iframe
-            src="https://www.youtube.com/embed/sEaKrsNeLfQ?enablejsapi=1&modestbranding=1&rel=0&showinfo=0&controls=1"
-            title="YouTube video"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/sEaKrsNeLfQ?enablejsapi=1&modestbranding=1&rel=0&showinfo=0&controls=1&autoplay=0&fs=1&iv_load_policy=3"
+          title="YouTube video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+
+
         </div>
       </div>
     </section>
