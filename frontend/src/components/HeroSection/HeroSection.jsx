0b7a3973e8d1380d98cfa3c0d4db2e5a48f8a7e4
Etczer <etczermerivil@gmail.com>
2025-02-15 19:37:54 -0500

    Replaced psycopg2 with psycopg2-binary for faster installation, Added <link rel=preload as=video> for hero video

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -14,8 +14,8 @@ const HeroSection = () => {
   return (
     <section className="hero-section">
       {/* Background Video */}
-      <video autoPlay loop muted playsInline className="hero-video">
-       <source src="/Videos/7.webm" type="video/webm"/>
+      <video autoPlay loop muted playsInline className="hero-video" preload="auto">
+        <source src="/Videos/7.webm" type="video/webm"/>
         <source src="/Videos/7.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
